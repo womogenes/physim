44786ec90e0e87f5f137ba928faec00ce0268854
William Y. Feng <womogenes2@gmail.com>
2024-11-13 13:14:17 -0500

    Attempt u-net training

--- u-net/unet_model.py
+++ u-net/unet_model.py
@@ -1,6 +1,7 @@
 """ Full assembly of the parts to form the complete network """
 
 from unet_parts import *
+from torch.nn import Linear
 
 
 class UNet(nn.Module):
@@ -21,6 +22,7 @@ class UNet(nn.Module):
         self.up3 = (Up(256, 128 // factor, bilinear))
         self.up4 = (Up(128, 64, bilinear))
         self.outc = (OutConv(64, n_classes))
+        # self.fc = (OutConv(32, n_classes))
 
     def forward(self, x):
         x1 = self.inc(x)
@@ -32,8 +34,9 @@ class UNet(nn.Module):
         x = self.up2(x, x3)
         x = self.up3(x, x2)
         x = self.up4(x, x1)
-        logits = self.outc(x)
-        return logits
+        x = self.outc(x)
+        # x = self.fc(x)
+        return x
 
     def use_checkpointing(self):
         self.inc = torch.utils.checkpoint(self.inc)
--- u-net/unet_parts.py
+++ u-net/unet_parts.py
@@ -15,7 +15,7 @@ class DoubleConv(nn.Module):
         self.double_conv = nn.Sequential(
             nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(mid_channels),
-            nn.Tanh(),
+            nn.ReLU(),
             nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU()
